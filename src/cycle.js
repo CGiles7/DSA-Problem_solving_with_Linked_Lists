784b840344d1902497099eec8fe755b5f124c426
Callie Giles <anchoredinthecrown@gmail.com>
2023-10-07 14:55:29 -0400

    update code

--- src/cycle.js
+++ src/cycle.js
@@ -1,3 +1,21 @@
-const cycle = (list) => {};
+const cycle = (list) => {
+    if (list.head == null) {
+        return false;
+    }
+
+    let slow = list.head;
+    let fast = list.head;
+
+    while (fast != null && fast.next != null) {
+        slow = slow.next;
+        fast = fast.next.next;
+
+        if (slow == fast) {
+            return true;
+        }
+    }
+
+    return false;
+};
 
 module.exports = cycle;
--- src/editor.js
+++ src/editor.js
@@ -19,7 +19,18 @@ class Editor {
    * @param {*} char a value to be inserted into the editor
    * @returns {Editor} a reference to this editor
    */
-  insert() {}
+  insert(char) {
+  if (this.cursor === null || this.text.length === 0) {
+    // If the cursor is null or the editor is empty, insert char as the new head
+    this.text.insertAtHead(char);
+    this.cursor = this.text.head;
+  } else {
+    // Insert char at the cursor position
+    this.text.insert(char, (node) => node === this.cursor);
+    this.cursor = this.cursor.next; // Move the cursor one position to the right
+  }
+  return this; // Return a reference to this editor
+}
 
   /**
    * Remove the character at the cursor position.
@@ -27,21 +38,80 @@ class Editor {
    * If editor is empty does nothing.
    * @returns {Editor} a reference to this editor
    */
-  delete() {}
+  delete() {
+  if (this.text.length === 0) {
+    // If the editor is empty, do nothing
+    return this;
+  }
+
+  if (this.cursor === null) {
+    // If the cursor is at the start of the text, do nothing
+    return this;
+  }
+
+  if (this.cursor === this.text.head) {
+    // If the cursor is at the head of the text, delete the head
+    this.text.head = this.text.head.next;
+    this.cursor = null;
+  } else {
+    // Find the previous node before the cursor
+    const previousNode = this.text.find((node) => node.next === this.cursor);
+
+    if (previousNode) {
+      // Remove the character currently indicated by the cursor
+      previousNode.next = this.cursor.next;
+      // Move the cursor to the previous character
+      this.cursor = previousNode;
+    }
+  }
+
+  return this; // Return a reference to this editor
+}
 
   /**
    * Moves the cursor one position to the left.
    * If the cursor is at the start of the editor nothing happens.
    * @returns {Editor} a reference to this editor
    */
-  arrowLeft() {}
+  arrowLeft() {
+  if (this.cursor === null) {
+    // If the cursor is null, do nothing
+    return this;
+  }
+
+  if (this.cursor === this.text.head) {
+    // If the cursor is at the head, move it before the head (null)
+    this.cursor = null;
+    return this;
+  }
+
+  // Find the previous node before the cursor
+  const previousNode = this.text.find((node) => node.next === this.cursor);
+
+  if (previousNode) {
+    // Move the cursor to the previous character
+    this.cursor = previousNode;
+  }
+
+  return this; // Return a reference to this editor
+}
 
   /**
    * Moves the cursor one position to the right.
    * If the cursor is at the end of the editor nothing happens.
    * @returns {Editor} a reference t this editor
    */
-  arrowRight() {}
+  arrowRight() {
+  if (this.cursor === null) {
+    // If the cursor is null, move it to the head
+    this.cursor = this.text.head;
+  } else if (this.cursor.next !== null) {
+    // If the cursor is not at the last character, move it to the next character
+    this.cursor = this.cursor.next;
+  }
+
+  return this; // Return a reference to this editor
+}
 }
 
 module.exports = Editor;
